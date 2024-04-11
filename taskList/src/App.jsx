43b18ef668351bde6d14dfe3b884c8f91452ae9f
Brasero <brandonricci4@gmail.com>
2024-04-11 12:19:11 +0200

    createAsyncThunk

--- taskList/src/App.jsx
+++ taskList/src/App.jsx
@@ -1,19 +1,11 @@
 import './App.css'
-import {useDispatch, useSelector} from "react-redux";
-import {increment, incrementOf, reset} from "./store/Slice/counterSlice.js";
+import TaskPage from "./page/Task/index.page.jsx";
 
 function App() {
 
-    const counter = useSelector(state => state.counter.count)
-    const dispatch = useDispatch()
-
-
-
   return (
     <>
-        <button onClick={( )=> dispatch(reset())}>Reset</button>
-        {counter}
-        <button onClick={() => dispatch(incrementOf({amount: 4}))}>Click</button>
+      <TaskPage />
     </>
   )
 }
--- taskList/src/store/index.js
+++ taskList/src/store/index.js
@@ -1,9 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import counterSlice from "./Slice/counterSlice.js";
+import taskSlice from "./Slice/taskSlice.js";
+import citiesSlice from "./Slice/citiesSlice.jsx";
 
 const store = configureStore({
     reducer: {
-        counter: counterSlice
+        counter: counterSlice,
+        tasks: taskSlice,
+        city: citiesSlice
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([
         //Middleware personnalisé
