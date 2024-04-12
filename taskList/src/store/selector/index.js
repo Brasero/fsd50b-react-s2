c88d393ba9ee4431cdb2d83ab1a292fd85b71df9
Brasero <brandonricci4@gmail.com>
2024-04-12 10:10:22 +0200

    enonce evaluation

--- taskList/src/store/selector/index.js
+++
@@ -1,7 +0,0 @@
-export const selectTaskTitle = state => state.tasks.task.title
-
-export const selectTasks = state => state.tasks.tasks
-
-export const selectTask = state => state.tasks.task
-
-export const selectApiStatus = state => state.apiStatus
